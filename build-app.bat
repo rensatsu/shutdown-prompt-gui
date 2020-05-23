34a63fa31f15e6794e38aa08171df45a49bdabf7
Pavel Tvaladze <reddragonsportal@gmail.com>
2020-05-23 20:29:54 +0300

    install dependencies without choco; cache deps

--- build-app.bat
+++ build-app.bat
@@ -1,10 +1,9 @@
 @echo off
-set PATH=%PATH%;C:\Program Files\AutoHotkey\Compiler
+set PATH=%PATH%;%~dp0\dependencies;C:\Program Files\AutoHotkey\Compiler
 set OUTPATH="dist"
 set OUTNAME="shutdown-prompt-gui.exe"
 set INNAME="shutdown-prompt-gui.ahk"
 set ICON="icon.ico"
 
-cd %~dp0
 IF NOT EXIST %OUTPATH% mkdir %OUTPATH%
 Ahk2Exe.exe /in %INNAME% /out %OUTPATH%/%OUTNAME% /icon %ICON%
--- build-icon.bat
+++ build-icon.bat
@@ -1,6 +1,6 @@
 @echo off
+set PATH=%PATH%;%~dp0\dependencies
 set ICON_SOURCE="icon.svg"
 set ICON_TARGET="icon.ico"
 
-cd %~dp0
 magick.exe convert -background none %ICON_SOURCE% -define icon:auto-resize %ICON_TARGET%
