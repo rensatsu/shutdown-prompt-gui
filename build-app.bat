578530923cace721bb6f747e3cfeb3ecf27fa2e9
Pavel Tvaladze <reddragonsportal@gmail.com>
2020-05-23 19:38:28 +0300

    prepare github actions build

--- build-app.bat
+++ build-app.bat
@@ -1,8 +1,10 @@
 @echo off
-set AHK2EXE="C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe"
+set PATH=%PATH%;C:\Program Files\AutoHotkey\Compiler
+set OUTPATH="dist"
 set OUTNAME="shutdown-prompt-gui.exe"
 set INNAME="shutdown-prompt-gui.ahk"
 set ICON="icon.ico"
 
 cd %~dp0
-%AHK2EXE% /in %INNAME% /out %OUTNAME% /icon %ICON%
+IF NOT EXIST %OUTPATH% mkdir %OUTPATH%
+Ahk2Exe.exe /in %INNAME% /out %OUTPATH%/%OUTNAME% /icon %ICON%
--- build-icon.bat
+++ build-icon.bat
@@ -1,7 +1,6 @@
 @echo off
-set MAGICK="magick"
 set ICON_SOURCE="icon.svg"
 set ICON_TARGET="icon.ico"
 
 cd %~dp0
-%MAGICK% convert -background none %ICON_SOURCE% -define icon:auto-resize %ICON_TARGET%
+magick.exe convert -background none %ICON_SOURCE% -define icon:auto-resize %ICON_TARGET%
